4ec188495edd822d9c15e6ded5ecbb0e4b92746f
joaojvf <joao.victorf02@hotmail.com>
2020-05-09 15:11:16 -0300

    Concluindo a versao

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -144,15 +144,13 @@ const SignIn = props => {
           <div className={classes.quote}>
             <div className={classes.quoteInner}>
               <Typography className={classes.quoteText} variant="h1">
-                Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
-                they sold out High Life.
+                Projeto criado utilizando React e Redux
               </Typography>
               <div className={classes.person}>
                 <Typography className={classes.name} variant="body1">
-                  Takamaru Ayako
+                  Estilização feita utilizando Material UI, utilizando o tema da Devias IO
                 </Typography>
                 <Typography className={classes.bio} variant="body2">
-                  Manager at inVision
                 </Typography>
               </div>
             </div>
